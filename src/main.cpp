52747fc74d086b464e9d0ac697c7186bb2e98ec6
Tailan Mendes <tailan.cardoso.eng@gmail.com>
2022-08-06 11:26:55 -0400

    Add informações para acesso a Blockchain

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,18 @@
 #define SSID     "2G_KZMNDS"
 #define WIFI_PASSWD "mnds190518"
 
+//INFURA
+#define INFURA_HOST "rinkeby.infura.io"
+#define INFURA_PATH "/v3/ea9cd517fe4c4782a5f7ee526a578ec5"
 
+/** METAMASK ADDRESS **/
+#define MY_ADDRESS "0x8047490dA302959AA294e9d096f2B0803140D63C"
+
+/** Contract Address **/
+#define CONTRACT_ADDRESS "0x194E4A079f507208B544eB8EAd2821e3Daa6a8C5"
+
+/** MetaMask Private Key **/
+const char* private_key = "8dafbc49423c21e71c778421e7a6b30fac6702390b47bd9e37e2969f5df1fd1d";
 
 int wifi_counter = 0;
 
