3f54d4afdfe8ab1f788d21539be5f353f88e10ae
Tailan Mendes <tailan.cardoso.eng@gmail.com>
2022-08-06 16:56:52 -0400

    Formatando dados coletados - formatDataToSent()

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <WiFi.h>
 #include <Web3.h>
 #include "ClosedCube_HDC1080.h"
+#include <iomanip>
+#include <sstream>
 
 //WIFI
 #define SSID     "2G_KZMNDS"
@@ -19,6 +21,9 @@
 /** Contract Address **/
 #define CONTRACT_ADDRESS "0x194E4A079f507208B544eB8EAd2821e3Daa6a8C5"
 
+/** Smart Contract Functions Signature **/
+#define SET_MEASURE "setMeasure(string)"
+
 /** MetaMask Private Key **/
 const char* private_key = "8dafbc49423c21e71c778421e7a6b30fac6702390b47bd9e37e2969f5df1fd1d";
 
@@ -30,6 +35,10 @@ double humidity;
 
 void setupWiFI();
 void sensorCollectData();
+void sendDataToSamartContract(string measure);
+string formatDataToSend();
+
+Web3 web3(INFURA_HOST, INFURA_PATH);
 
 void setup() 
 {
@@ -47,13 +56,15 @@ void setup()
 void loop() 
 {
   sensorCollectData();
+  Serial.println(formatDataToSend().c_str());
 
+  /*
   Serial.print("T=");
   Serial.print(temperature);
   Serial.print("C, RH=");
 	Serial.print(humidity);
 	Serial.println("%");
-
+  */
   delay(COLLECT_TIME);
 }
 
@@ -106,4 +117,37 @@ void sensorCollectData()
 {
     temperature = hdc1080.readTemperature();
     humidity = hdc1080.readHumidity();
+}
+
+string formatDataToSend()
+{
+  stringstream fmeasure;
+
+  // Converting double to string and setting precision 2 decimal
+  fmeasure << std::fixed << std::setprecision(2) << temperature << "|" << humidity;
+
+  //sensor_data = to_string(temperature) + "|" + to_string(humidity); // timestamp
+
+  return fmeasure.str();;
+}
+
+void sendDataToSamartContract(string measure)
+{
+  Contract contract(&web3, CONTRACT_ADDRESS);
+  contract.SetPrivateKey(private_key);
+  string addr = MY_ADDRESS;
+  uint32_t nonceVal = (uint32_t)web3.EthGetTransactionCount(&addr);
+
+  /** Gas settings - REVIEW **/
+  unsigned long long gasPriceVal = 2500000008ULL;
+  uint32_t  gasLimitVal = 80000;
+
+  //string p = contract.SetupContractData("setMeasure(string)", measure);
+  string p = contract.SetupContractData(SET_MEASURE, measure);
+
+  string contractAddr = CONTRACT_ADDRESS;
+  uint256_t weiValue = 000000000000000;
+
+  string result = contract.SendTransaction(nonceVal, gasPriceVal, gasLimitVal, &contractAddr, &weiValue, &p);
+
 }
